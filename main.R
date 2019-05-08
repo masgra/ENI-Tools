f7ca9f573b2f5aaabe2335f1807db7d323f04283
grafma <martin.graf@ufz.de>
2019-05-08 19:32:26 +0200

    Parameter optimization for Sice.easi

--- main.R
+++ main.R
@@ -1,13 +1,13 @@
 # isntall required packages
-install.packages("igraph") # network analysis tool
-install.packages("ggraph") # network analysis tool
-install.packages("vegan")  # pairwise dissimilarity 
-install.packages("MCL")    # cluster detection
+# install.packages("igraph") # network analysis tool
+# install.packages("ggraph") # network analysis tool
+# install.packages("vegan")  # pairwise dissimilarity 
+# install.packages("MCL")    # cluster detection
 
 # Install SpiecEasi package
-install.packages("devtools")
+# install.packages("devtools")
 library(devtools)
-install_github("zdk123/SpiecEasi", force = TRUE)
+# install_github("zdk123/SpiecEasi", force = TRUE)
 
 library(SpiecEasi)
 library(igraph)
@@ -35,18 +35,27 @@ Reactor = list(R.R = c("S-3", "S-22", "S-27"),
 
 # quick and dirty subset selection 
 ds <- ds.all[,which(colnames(ds.all) %in% Reactor$R.C)]
-otu.sub <- ds[which( rownames(ds) %in% names(sort(rowSums(ds),decreasing=T))[1:200]),]
+otu.sub <- ds[which( rownames(ds) %in% names(sort(rowSums(ds),decreasing=T))[1:500]),]
 
 
 # Spiec easy: non-normalized count OTU/data table with samples on rows and features/OTUs in columns
-
-SE.glasso  <- spiec.easi(t(otu.sub), nlambda=10, method = "glasso", verbose = T, pulsar.params=list(rep.num=10, ncores=1), lambda.min.ratio=1e-4)
-SE.mb      <- spiec.easi(t(otu.sub), nlambda=10, method = "mb", verbose = T, pulsar.params=list(rep.num=10, ncores=1), lambda.min.ratio=1e-4)
+ptm <- proc.time()
+SE.glasso  <- spiec.easi(t(otu.sub), nlambda=10, method = "glasso", verbose = T, pulsar.params=list(rep.num=10, ncores=1), lambda.min.ratio=1e-)
+proc.time() - ptm
+ptm <- proc.time()
+SE.mb      <- spiec.easi(t(otu.sub), nlambda=100, method = "mb", verbose = T, pulsar.params=list(rep.num=10, ncores=1), lambda.min.ratio=1e-4)
+proc.time() - ptm
 SE.sparcc  <- sparcc(t(otu.sub))
+
+getOptInd(SE.mb)
+getStability(SE.mb) # default target stability is 0.05 
+
+sum(getRefit(SE.mb))/2
+
 
 
 ## Define arbitrary threshold for SparCC correlation matrix for the graph
-sparcc.graph <- ((abs(SE.sparcc$Cor) >= 0.) & (abs(SE.sparcc$Cor) <= 0.99))
+sparcc.graph <- ((abs(SE.sparcc$Cor) >= 0.3) & (abs(SE.sparcc$Cor) <= 0.99))
 diag(sparcc.graph) <- 0
 sparcc.graph <- Matrix(sparcc.graph, sparse=TRUE)
 
@@ -58,9 +67,10 @@ ig.sparcc     <- adj2igraph(sparcc.graph)
 # Visualize using igraph plotting
 
 ## set size of vertex proportional to clr-mean
-vsize    <- rowMeans(clr(otu.sub, 1))+6
+vsize    <- rowMeans(clr(otu.sub, 1))+4.5
 am.coord <- layout.fruchterman.reingold(ig.glasso)
-
+am.coord <- layout.fruchterman.reingold(ig.sparcc)
+am.coord <- layout.fruchterman.reingold(ig.mb)
 
 par(mfrow=c(1,3))
 plot(ig.glasso, layout=am.coord, vertex.size=vsize, vertex.label=NA, main="glasso")
